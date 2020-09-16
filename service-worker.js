aeccdc59eb036f6e90ebb03b7b533e406e2a80ef
Paavlo <pavlo.com.5@gmail.com>
2020-09-16 15:33:56 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9b9b912321e3d315fc8f540d885eee7f.js"
+  "./precache-manifest.32e0c222de215136188b5e258121d999.js"
 );
 
 self.addEventListener('message', (event) => {
